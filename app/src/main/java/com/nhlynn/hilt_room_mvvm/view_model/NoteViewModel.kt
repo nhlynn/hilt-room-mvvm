4b528fa733be80366d71f3beb787776c2f999ac3
nhlynn <nayhtetlynn.git@gamil.com>
2022-05-31 14:26:19 +0630

    room mvvm with dagger hilt and coroutine

--- app/src/main/java/com/nhlynn/hilt_room_mvvm/view_model/NoteViewModel.kt
+++ app/src/main/java/com/nhlynn/hilt_room_mvvm/view_model/NoteViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nhlynn.hilt_room_mvvm.db.NoteEntity
 import com.nhlynn.hilt_room_mvvm.respsitory.UserRepository
-import dagger.Provides
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
